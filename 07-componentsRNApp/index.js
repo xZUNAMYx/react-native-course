f17a8af6df3ed051640e26479a5977a124ecd3b8
AndresAC <andres_fel1pe@hotmail.com>
2024-10-07 00:02:55 -0500

    Creacion del proyecto 07-componentsRNApp e inici de seccion 12

---
+++ 07-componentsRNApp/index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import {name as appName} from './app.json';
+import { ComponentsRNApp } from './src/ComponentsRNApp';
+
+AppRegistry.registerComponent(appName, () => ComponentsRNApp);
---
+++ 07-componentsRNApp/src/ComponentsRNApp.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+import React from 'react';
+
+export const ComponentsRNApp = () => {
+  return (
+    <View>
+      <Text>ComponentsRNApp</Text>
+    </View>
+  );
+};
